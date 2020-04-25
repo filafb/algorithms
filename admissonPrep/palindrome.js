dc6cdfd0c43365fb835d3f64ce833282c7c45797
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-04-25 11:40:49 -0400

    recursive palindrome

--- admissonPrep/palindrome.js
+++ admissonPrep/palindrome.js
@@ -14,3 +14,14 @@ function palindrome(str) {
   }
   return "true"
 }
+
+
+function palindromeRec(str, left = 0, right = str.length -1) {
+    if(left > right) {
+      return "true"
+    } else if(str[left].toLowerCase() !== str[right].toLowerCase()) {
+      return "false"
+    } else {
+      return palindromeRec(str, ++left, --right)
+    }
+}
